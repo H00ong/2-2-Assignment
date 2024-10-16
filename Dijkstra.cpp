f03775170951a2a716e41b78b7f7bddea1aac086
Park Jae Hong <154869827+H00ong@users.noreply.github.com>
2024-10-16 11:14:53 +0900

    algo week6 Dijkstra

---
+++ Dijkstra.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
+
+using namespace std;
+using pr = pair<int, int>;
+
+vector<pr> adj[10000];
+map<int, int> result;
+bool isInTree[10000];
+
+int main() 
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int vertexCount, edgeCount;
+
+	cin >> vertexCount >> edgeCount;
+
+	while (edgeCount--) {
+		int edge_start, edge_end, weight;
+		cin >> edge_start >> edge_end >> weight;
+		adj[edge_start].push_back({ weight, edge_end });
+		adj[edge_end].push_back({ weight, edge_start });
+	}
+
+	int count = 0;
+	isInTree[0] = true;
+	priority_queue<pr, vector<pr>, greater<pr>> pq;
+
+	for (auto elem : adj[0]) 
+		pq.push(make_pair(elem.first, elem.second));
+
+	while (count < vertexCount - 1) 
+	{
+		auto var = pq.top(); pq.pop();
+
+		if (isInTree[var.second]) continue;
+
+		isInTree[var.second] = true;
+
+		count++;
+
+		result[var.second] = var.first;
+
+		for (auto elem : adj[var.second])
+		{
+			if (!isInTree[elem.second])
+			{
+				pq.push(make_pair(elem.first + var.first, elem.second));
+			}
+		}
+	}
+
+	for (auto elem : result) 
+	{
+		cout << elem.first << " " << elem.second << endl;
+	}
+
+	return 0;
+}
